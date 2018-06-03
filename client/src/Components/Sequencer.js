4c6230bf6474447c449a7640fce411b9abc4c3ed
Jade Westover <jade424433@gmail.com>
2018-06-02 23:03:21 -0700

    Some testing

--- client/src/Components/Sequencer.js
+++ client/src/Components/Sequencer.js
@@ -1,41 +1,41 @@
 import React, { Component } from 'react';
 import './Sequencer.css';
+import ApplicationData from "../Models/ApplicationData";
 
 class Sequencer extends Component {
+    constructor() {
+        super();
+        this.state = { isTrue: false };
+        this.createSequencer = this.createSequencer.bind(this);
+    }
+
+    createSequencer() {
+        this.setState(state => ({
+            isTrue: !state.isTrue
+        }));
+    }  
+
+    testButton() {
+        let isTrue = this.state.isTrue;
+        if (!isTrue) {
+            return (
+                <h1>thing is false</h1>
+            )
+        } else {
+            return (
+                <h1>thing is true</h1>
+            )
+        }
+    }
+
     render() {
         return (
             <div>
                 <div className="container topPadding">
                     <div className="jumbotron">
                         <div className="row rowMargin">
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                        </div>
-                        <div className="row rowMargin">
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
-                            <div className="sequencerButton"></div>
+                            <div className="sequencerButton" onClick={this.createSequencer}></div>
+                            {this.testButton()}
                         </div>
                     </div>
                 </div>
--- client/src/Models/ApplicationData.js
+++ client/src/Models/ApplicationData.js
@@ -1,5 +1,7 @@
 class ApplicationData {
     constructor() {
-        
+        this.sequencer = [];
     }
 }
+
+export default ApplicationData;
